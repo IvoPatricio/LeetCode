ba7867d2958ca1cb6f98bd56e8de086277963a14
Ivo Marques <132523498+IvoPatricio@users.noreply.github.com>
2024-09-18 16:24:56 +0100

    Time: 4 ms (88.89%), Space: 25 MB (31.17%) - LeetHub

---
+++ 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool countBits(int num, int k)
+    {
+        int count = 0;
+        while(num > 0)
+        {
+            count += (num & 1);
+            num >>= 1;
+        }
+        if (count == k)
+            return true;
+        return false;
+    }
+    int sumIndicesWithKSetBits(vector<int>& nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (countBits(i, k) == true)
+                sum += nums[i];
+        }
+        return sum;
+    }
+};
