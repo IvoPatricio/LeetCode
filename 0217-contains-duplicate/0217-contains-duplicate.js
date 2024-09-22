ecfabde72057af821e1ae646cafe09aca4cd6d95
Ivo Marques <132523498+IvoPatricio@users.noreply.github.com>
2024-09-22 15:42:17 +0100

    Time: 87 ms (31.89%), Space: 60.8 MB (75.43%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let numsSet = new Set();
+
+    for (let num of nums)
+    {
+        if (numsSet.has(num))
+        {
+            return true;
+        }
+        numsSet.add(num);
+    }
+    return false;
+};
