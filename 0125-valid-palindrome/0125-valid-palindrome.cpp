63f6a84e8df22d49c58ba43e6d6ff26da4a5d84f
Ivo Marques <132523498+IvoPatricio@users.noreply.github.com>
2024-09-25 13:48:46 +0100

    Time: 24 ms (5.39%), Space: 9.3 MB (30.86%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        std::string str = "";
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] != ' ')
+            {
+                if (s[i] >= 'A' && s[i] <= 'Z')
+                    str += s[i] + 32;
+                else if (s[i] >= 'a' && s[i] <= 'z')
+                    str += s[i];
+                else if (s[i] >= '0' && s[i] <= '9')
+                    str += s[i];
+            }
+        }
+        int x = str.size() - 1;
+        for (int i = 0; i < str.size() / 2; i++)
+        {
+            std::cout << str[i] << "---" << str[x] << std::endl;
+            if (str[i] == str[x])
+                x--;
+            else
+                return false;
+        }
+        return true;
+    }
+};
