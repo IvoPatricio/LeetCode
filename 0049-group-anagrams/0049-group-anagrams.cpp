5d3c4327f5d7830c4220d5796c3e84352ef65ee0
Ivo Marques <132523498+IvoPatricio@users.noreply.github.com>
2024-09-25 13:23:14 +0100

    Time: 24 ms (79.5%), Space: 23.8 MB (89.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs)
     {
-        unordered_map<string, vector<string>> SortedMap;
+        std::unordered_map<std::string, std::vector<string>> freqStrs;
         vector<vector<string>> result;
         for (int i = 0; i < strs.size(); i++)
         {
-            string strSort = strs[i];
-            sort(strSort.begin(), strSort.end());
-            SortedMap[strSort].push_back(strs[i]);
+            std::string str = strs[i];
+            sort(str.begin(), str.end());
+            freqStrs[str].push_back(strs[i]);
         }
-        for (auto it = SortedMap.begin(); it != SortedMap.end(); it++)
+        for (auto it = freqStrs.begin(); it != freqStrs.end(); it++)
         {
             result.push_back(it->second);
         }
